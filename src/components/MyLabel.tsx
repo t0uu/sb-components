d2592a75edb99f9ade3e92f14a9b85231b1b6424
t0uu <srtouzet@hotmail.com>
2023-02-23 11:46:27 -0300

    Chromatic Agregado

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -15,13 +15,16 @@ AllCaps: boolean
 
 color: 'text-primary' | 'text-secondary' | 'text-tertiary'
 
-fontColor: '#5517ac'
+fontColor: string;
+
+backgroundColor?: string;
+
 }
 
 
-export const MyLabel = ({label = 'No Label' ,size = 'normal',AllCaps = false,color = 'text-primary', fontColor }: Props) => {
+export const MyLabel = ({label = 'No Label' ,size = 'normal',AllCaps = false,color = 'text-primary', fontColor,backgroundColor = 'transparent' }: Props) => {
   return ( 
-    <span className={` label ${size} ${color} ${AllCaps && `text-uppercase`} `} style={{backgroundColor: `${fontColor}`}}>
+    <span className={` label ${size} ${color} ${AllCaps && `text-uppercase`} `} style={{color: fontColor,backgroundColor}}>
         {label}
     </span>
     )
